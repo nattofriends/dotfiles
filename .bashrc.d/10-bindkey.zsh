221a8b3ddc4c818ae89b80bfcefd03cd6e5a3279
nattofriends <nattofriends@gmail.com>
2023-10-02 14:35:20 -0700

    Remove duplicate function parts, add ^X^E binding for zsh

---
+++ .bashrc.d/10-bindkey.zsh
@@ -0,0 +1,16 @@
+# These are controlled by .inputrc when using bash
+
+# Reset to Emacs mode, which is way more popular and normal for shells
+bindkey -e
+
+# Make macOS Home and End keys work through SSH
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
+# and locally (???)
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+
+# https://stackoverflow.com/questions/890620/unable-to-have-bash-like-c-x-e-in-zsh
+autoload edit-command-line
+zle -N edit-command-line
+bindkey '^X^e' edit-command-line
