161adac6909e7817ed93eb624b9c09fb129e3d8c
nattofriends <nattofriends@gmail.com>
2024-05-21 15:48:50 -0700

    pyenv

--- .bashrc.d/89-pyenv.sh
+++ .bashrc.d/89-pyenv.sh
@@ -1,4 +1,6 @@
-if command -v pyenv >/dev/null; then
+# See also: pyenv bootstrap in metaenv
+if [ -d $HOME/.pyenv ]; then
+  pathappend $HOME/.pyenv/bin
 
   export PYENV_ROOT=$HOME/.pyenv
   eval "$(pyenv init -)"
@@ -6,5 +8,4 @@ if command -v pyenv >/dev/null; then
   pyenv-install () {
     env PYTHON_CONFIGURE_OPTS='--enable-optimizations --with-lto' PYTHON_CFLAGS='-march=native -mtune=native' PROFILE_TASK='-m test.regrtest --pgo -j0' pyenv install "$@"
   }
-
 fi
