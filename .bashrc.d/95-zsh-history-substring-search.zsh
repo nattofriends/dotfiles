a60f9e7ae86001353f6bb822a708f0b4acd490fb
nattofriends <nattofriends@gmail.com>
2025-11-01 13:11:57 -0700

    update zsh plugins configuration

--- .bashrc.d/95-zsh-history-substring-search.zsh
+++ .bashrc.d/95-zsh-history-substring-search.zsh
@@ -1,2 +1,7 @@
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+# If the plugin isn't present, don't trample on the builtin bindings
+
+zle -la history-substring-search-up && \
+    bindkey '^[[A' history-substring-search-up
+
+zle -la history-substring-search-down && \
+    bindkey '^[[B' history-substring-search-down
