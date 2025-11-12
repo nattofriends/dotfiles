365aaf10d2d1679946ee948792f2959fd26f5e3d
nattofriends <nattofriends@gmail.com>
2025-11-11 23:24:48 -0800

    support lockdiff

--- .metaconf/git/50-diff.sh
+++ .metaconf/git/50-diff.sh
@@ -2,4 +2,11 @@ if command -v difft >/dev/null; then
     rc_debug "enabling difftastic"
     cat difftastic-config >> generated/config
 fi
+
+if command -v lockdiff >/dev/null; then
+    rc_debug "enabling lockdiff"
+    cat lockdiff-config >> generated/config
+    cat lockdiff-attributes >> generated/attributes
+fi
+
 # TODO: Configure diff-highlight
