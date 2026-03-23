17f14e0b4adc76ac63f04603abf81a9d4619cde6
nattofriends <nattofriends@gmail.com>
2026-03-22 18:07:38 -0700

    shorter way of promot char in zsh
    (cherry picked from commit bbbae32129c34172d8b6224f523f99587d8033d1)

--- .bashrc.d/20-statements.zsh
+++ .bashrc.d/20-statements.zsh
@@ -4,10 +4,4 @@ setopt PROMPT_SUBST
 # Would be nice if we could use __git_ps1 and friends but this is fine for now...
 # git_branch is defined in 10-function
 
-if [[ "$EUID" == "0" ]]; then
-    PROMPT_CHAR="#"
-else
-    PROMPT_CHAR="$"
-fi
-
-PS1='[%D{%m/%d %R:%S}] %B%~ $(git_branch)${PROMPT_CHAR} %b'
+PS1='[%D{%m/%d %R:%S}] %B%~ $(git_branch)%(!.#.$) %b'
