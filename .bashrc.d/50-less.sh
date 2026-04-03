1235b3350d4badd8f1ca722d4f107df42f1e69d4
nattofriends <nattofriends@gmail.com>
2026-04-02 18:18:25 -0700

    fix grep invocation

--- .bashrc.d/50-less.sh
+++ .bashrc.d/50-less.sh
@@ -3,6 +3,7 @@ configure_less () {
     mkdir -p $cachedir
 
     if [[ "$RC_CACHING" = 1 ]] && [[ -f ${cachedir}/less ]]; then
+        rc_debug "less: using cached configuration"
         source ${cachedir}/less
         return
     fi
@@ -11,9 +12,12 @@ configure_less () {
     # https://unix.stackexchange.com/questions/184597/
     local less="-XRF --mouse"
 
-    local version=$(less --version | head -n 1 | grep -o "\d.*\d")
+    local version=$(less --version | head -n 1 | grep -Eo "[[:digit:]].*[[:digit:]]")
+    rc_debug "less: version is ${version}"
+
     # feature added in less 574
     if $(compare_version 574 $version); then
+        rc_debug "less: enabling --incsearch"
         less="${less} --incsearch"
     fi
     export LESS=$less
