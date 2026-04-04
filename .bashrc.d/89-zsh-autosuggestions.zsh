101c6f3a180615d77bd9dfe56d0122f825885e9e
nattofriends <nattofriends@gmail.com>
2026-04-03 21:38:40 -0700

    fix a bunch of stuff about completion
    (cherry picked from commit 52c3981c1f55aba7e1aa66bb00a687ca6a5ea330)

--- .bashrc.d/89-zsh-autosuggestions.zsh
+++ .bashrc.d/89-zsh-autosuggestions.zsh
@@ -1,7 +1,13 @@
-export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-export ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
-export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
-export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+ZSH_AUTOSUGGEST_IGNORE_WIDGETS+=(expand-or-complete)
+
+if command -v atuin >/dev/null; then
+    ZSH_AUTOSUGGEST_STRATEGY=(atuin)
+else
+    ZSH_AUTOSUGGEST_STRATEGY=(history)
+fi
 
 # Double tab to accept autosuggest
 zle -la autosuggest-accept && \
--- .bashrc.d/95-completion.zsh
+++ .bashrc.d/95-completion.zsh
@@ -4,29 +4,48 @@ fi
 
 autoload -Uz compinit bashcompinit
 
-if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+for dump in ~/.zcompdump(N.mh+24); do
     compinit -u
-else
-    compinit -u -C
-fi
+done
 
-bashcompinit
+compinit -C
 
-if [[ ! -d ~/.zsh/cache ]]; then
-    mkdir -p ~/.zsh/cache
-fi
+mkdir -p ~/.zsh/cache
 
 # Shift-Tab
 bindkey '^[[Z' reverse-menu-complete
 
+# Visual
+# Enable menu
+zstyle ':completion:*' menu select
+# Colorize
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} ""
+# Group items
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:descriptions' format '%B%F{yellow}-- %d --%f%b'
+zstyle ':completion:*' select-prompt %S%p%s
+zstyle ':completion:*:warnings' format '%F{red}no matches for:%f %d'
+
+# Matching
 zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 zstyle ':completion::complete:*' gain-privileges 1
 zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' menu select
+# Case insensitive and fuzzy
+zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' '+r:|[_-]=* r:|=*' '+l:|=*'
+# Completion order
+zstyle ':completion:*' completer _expand _complete _approximate
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# make: only show targets
 zstyle ':completion:*:make:*:targets' call-command true
 zstyle ':completion:*:*:make:*' tag-order 'targets'
+
+# git
+zstyle ':completion:*:*:git:*' factor 0.1
+
 setopt MENU_COMPLETE
 setopt AUTO_LIST
 setopt COMPLETE_IN_WORD
+setopt ALWAYS_TO_END
