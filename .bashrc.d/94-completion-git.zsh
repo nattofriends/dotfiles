63c1cb278e9c109175bd1af6949481b556089ee4
nattofriends <nattofriends@gmail.com>
2026-04-05 14:38:54 -0700

    fix up downloading bash completion

--- .bashrc.d/94-completion-git.zsh
+++ .bashrc.d/94-completion-git.zsh
@@ -1,13 +1,25 @@
-if command -v git >/dev/null; then
-    if [[ -e /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash ]]; then
-        zstyle ':completion:*:*:git:*' script /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
-    else
-        (
-            if [[ ! -e ~/.cache/.bashrc.d/git-completion.bash ]]; then
-                mkdir -p ~/.cache/.bashrc.d
-                curl -o ~/.cache/.bashrc.d/git-completion.bash --silent --compressed \
-                    https://raw.githubusercontent.com/felipec/git-completion/refs/heads/master/src/git_completion.bash
-            fi
-        ) &!
+configure_completion_git () {
+    # See also .metaconf/zsh-completions/50-git.sh
+
+    if (( $+commands[git] )); then
+        if [[ -e /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash ]]; then
+            zstyle ':completion:*:*:git:*' script /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
+        else
+            local cachedir=~/.cache/.bashrc.d
+            local cachefile=$cachedir/git-completion.bash
+            zstyle ':completion:*:*:git:*' script $cachefile
+            (
+                if [[ ! -e $cachefile ]]; then
+                    {
+                        [[ -d "$cachedir" ]] || mkdir -p "$cachedir"
+                        curl -L -o $cachefile -s --compressed \
+                            https://raw.githubusercontent.com/felipec/git-completion/refs/heads/master/src/git-completion.bash
+                    } &!
+                fi
+            ) &!
+        fi
     fi
-fi
+}
+
+configure_completion_git
+unset configure_completion_git
--- .metaconf/zsh-completions/50-git.sh
+++ .metaconf/zsh-completions/50-git.sh
@@ -1,10 +1,12 @@
+# See also .bashrc.d/94-completion-git.sh
+
 if command -v git >/dev/null; then
     # Should do this in a better way
     if [[ ! -e generated/_git ]]; then
         if [[ -e /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.zsh ]]; then
             ln -sf /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.zsh generated/_git
         else
-            curl -o generated/_git -s --compressed https://raw.githubusercontent.com/felipec/git-completion/refs/heads/master/src/_git
+            curl -L -o generated/_git -s --compressed https://raw.githubusercontent.com/felipec/git-completion/refs/heads/master/src/_git
         fi
     fi
 fi
