2dc28d194ba9fe54e44d277edc57d4245231ff63
nattofriends <nattofriends@gmail.com>
2026-04-07 20:40:35 -0700

    other autosuggest config
    (cherry picked from commit 85df92fed54774a0fc01f659b045678d19efe73c)

--- .bashrc.d/89-zsh-autosuggestions.zsh
+++ .bashrc.d/89-zsh-autosuggestions.zsh
@@ -1,7 +1,8 @@
 # This configuration is set before zsh-autosuggestions loads
 
 # No atuin even if it exists
-ZSH_AUTOSUGGEST_STRATEGY=(history)
+ZSH_AUTOSUGGEST_STRATEGY=(history match_prev_cmd)
 ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
+ZSH_AUTOSUGGEST_MIN_BUFFER_SIZE=3
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
