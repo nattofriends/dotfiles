cbb34b72524b450d1c75213cdc30dc426f17875f
nattofriends <nattofriends@gmail.com>
2026-04-22 20:52:13 -0700

    try using vsc_info for ps1 on zsh
    (cherry picked from commit 1d7c8b2f08be848ac4d5a2703878825a1723d177)

---
+++ .bashrc.d/20-ps1.zsh
@@ -0,0 +1,26 @@
+# https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#Prompt-Expansion
+
+autoload -Uz vcs_info
+autoload -Uz add-zsh-hook
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:*' formats '(%b) '
+
+vcs_info_precmd() {
+  local git_root=$(git rev-parse --show-toplevel 2>/dev/null)
+  [[ -z "$git_root" ]] && { vcs_info_msg_0_=""; return; }
+
+  if [[ "$git_root" == "$HOME" ]]; then
+    vcs_info_msg_0_=""
+    return
+  fi
+
+  vcs_info
+}
+
+add-zsh-hook precmd vcs_info_precmd
+
+setopt PROMPT_SUBST
+
+# No longer using git_branch
+PS1='%b[%D{%m/%d %R:%S}] %B%~ ${vcs_info_msg_0_}%(!.#.$)%{%f%b%} '
--- .bashrc.d/20-statements.zsh
+++
@@ -1,7 +0,0 @@
-# https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#Prompt-Expansion
-
-setopt PROMPT_SUBST
-# Would be nice if we could use __git_ps1 and friends but this is fine for now...
-# git_branch is defined in 10-function
-
-PS1='%b[%D{%m/%d %R:%S}] %B%~ $(git_branch)%(!.#.$) %b'
