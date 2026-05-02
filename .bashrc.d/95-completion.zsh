4961aec675b1f838945bb9a2e03c911ee3b5ef5e
nattofriends <nattofriends@gmail.com>
2026-05-01 17:48:34 -0700

    fix up completion configuration; run in block
    (cherry picked from commit fa9fdbc2ab579310961d5a2a8e12c78cbdde566a)

--- .bashrc.d/95-completion.zsh
+++ .bashrc.d/95-completion.zsh
@@ -1,57 +1,61 @@
-if [[ -d ~/.metaconf/zsh-completions/generated ]]; then
-    fpath=(~/.metaconf/zsh-completions/generated $fpath)
-fi
+() {
+    setopt local_options extended_glob
 
-autoload -Uz compinit bashcompinit
+    if [[ -d ~/.metaconf/zsh-completions/generated ]]; then
+        fpath=(~/.metaconf/zsh-completions/generated $fpath)
+    fi
 
-if [[ -s ~/.zcompdump && -n ~/.zcompdump(N.mh-24) ]]; then
-    rc_debug "loading cached completions"
-    compinit -C -u -d ~/.zcompdump
-else
-    rc_debug "loading completions normally"
-    compinit -u -d ~/.zcompdump
-fi
+    autoload -Uz compinit bashcompinit
 
-mkdir -p ~/.zsh/cache
+    if [[ -s ~/.zcompdump && -n ~/.zcompdump(N.mh-24) ]]; then
+        rc_log "loading completions (cached)"
+        compinit -C -u -d ~/.zcompdump
+    else
+        rc_log "loading completions"
+        compinit -u -d ~/.zcompdump
+    fi
 
-# Shift-Tab - reverse in completion menu, but accept autosuggest otherwise
-zmodload -i zsh/complist
+    mkdir -p ~/.zsh/cache
 
-bindkey -M menuselect '^[[Z' reverse-menu-complete
+    zmodload -i zsh/complist
 
-if (( $+widgets[autosuggest-accept] )); then
-    bindkey '^[[Z' autosuggest-accept
-fi
+    # Shift-Tab - reverse in completion menu, but accept autosuggest otherwise
+    bindkey -M menuselect '^[[Z' reverse-menu-complete
 
-# Visual
-# Enable menu
-zstyle ':completion:*' menu select
-# Colorize
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} ""
-# Group items
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*:descriptions' format '%B%F{yellow}%d%f%b'
-zstyle ':completion:*' select-prompt %S%p%s
-zstyle ':completion:*:warnings' format '%F{red}no matches for:%f %d'
+    if (( $+widgets[autosuggest-accept] )); then
+        bindkey '^[[Z' autosuggest-accept
+    fi
 
-# Matching
-zstyle ':completion:*' accept-exact '*(N)'
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path ~/.zsh/cache
-zstyle ':completion::complete:*' gain-privileges 1
-zstyle ':completion:*' squeeze-slashes true
-# Case insensitive and fuzzy
-zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' '+r:|[_-]=* r:|=*' '+l:|=*'
-# Completion order
-zstyle ':completion:*' completer _expand _complete _approximate
-zstyle ':completion:*:approximate:*' max-errors 1 numeric
+    # Visual
+    # Enable menu
+    zstyle ':completion:*' menu select
+    # Colorize
+    zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} ""
+    # Group items
+    zstyle ':completion:*' group-name ''
+    zstyle ':completion:*:descriptions' format '%B%F{yellow}%d%f%b'
+    zstyle ':completion:*' select-prompt %S%p%s
+    zstyle ':completion:*:warnings' format '%F{red}no matches for:%f %d'
 
-# make: only show targets
-zstyle ':completion:*:make:*:targets' call-command true
-zstyle ':completion:*:*:make:*' tag-order 'targets'
+    # Matching
+    zstyle ':completion:*' accept-exact '*(N)'
+    zstyle ':completion:*' use-cache on
+    zstyle ':completion:*' cache-path ~/.zsh/cache
+    zstyle ':completion::complete:*' gain-privileges 1
+    zstyle ':completion:*' squeeze-slashes true
+    # Case insensitive and fuzzy
+    zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]}' '+r:|[_-]=* r:|=*' '+l:|=*'
+    # Completion order
+    zstyle ':completion:*' completer _expand _complete _approximate
+    zstyle ':completion:*:approximate:*' max-errors 1 numeric
 
-# git
-zstyle ':completion:*:*:git:*' factor 0.1
+    # make: only show targets
+    zstyle ':completion:*:make:*:targets' call-command true
+    zstyle ':completion:*:*:make:*' tag-order 'targets'
+
+    # git
+    zstyle ':completion:*:*:git:*' factor 0.1
+}
 
 setopt MENU_COMPLETE
 setopt AUTO_LIST
