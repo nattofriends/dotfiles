dd750c67bdec5f3eeabb86b53bf68f279b97dea5
nattofriends <nattofriends@gmail.com>
2026-08-12 00:26:41 -0700

    Stop using vcs_info entirely
    (cherry picked from commit fadb79fe6f02e2555c6d74d2c6bb2d75752ec1bf)

--- .bashrc.d/20-ps1.zsh
+++ .bashrc.d/20-ps1.zsh
@@ -1,27 +1,40 @@
 # https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#Prompt-Expansion
 
-autoload -Uz vcs_info
 autoload -Uz add-zsh-hook
 
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git:*' formats '(%b) '
-zstyle ':vcs_info:git:*' actionformats '(%b|%a) '
+for _git in /opt/homebrew/bin/git /usr/bin/git; do
+  [[ -x $_git ]] && break
+done
 
-vcs_info_precmd() {
-  local git_root=$(git rev-parse --show-toplevel 2>/dev/null)
-  [[ -z "$git_root" ]] && { vcs_info_msg_0_=""; return; }
+_git_prompt=""
+typeset -gA _git_prompt_toplevel_cache
 
-  if [[ "$git_root" == "$HOME" ]]; then
-    vcs_info_msg_0_=""
-    return
+_git_prompt_precmd() {
+  local git_root
+  if (( ${+_git_prompt_toplevel_cache[$PWD]} )); then
+    git_root=${_git_prompt_toplevel_cache[$PWD]}
+  else
+    git_root=$($_git rev-parse --show-toplevel 2>/dev/null)
+    _git_prompt_toplevel_cache[$PWD]=$git_root
   fi
+  [[ -z "$git_root" || "$git_root" == "$HOME" ]] && { _git_prompt=""; return; }
 
-  vcs_info
+  local branch=$($_git symbolic-ref --short HEAD 2>/dev/null) || branch="detached"
+  local git_dir=${git_root}/.git
+
+  if [[ -d "$git_dir/rebase-merge" || -d "$git_dir/rebase-apply" ]]; then
+    _git_prompt="($branch|rebase) "
+  elif [[ -f "$git_dir/MERGE_HEAD" ]]; then
+    _git_prompt="($branch|merge) "
+  elif [[ -f "$git_dir/CHERRY_PICK_HEAD" ]]; then
+    _git_prompt="($branch|cherry-pick) "
+  else
+    _git_prompt="($branch) "
+  fi
 }
 
-add-zsh-hook precmd vcs_info_precmd
+add-zsh-hook precmd _git_prompt_precmd
 
 setopt PROMPT_SUBST
 
-# No longer using git_branch
-PS1='%b[%D{%m/%d %R:%S}] %B%~ ${vcs_info_msg_0_}%(!.#.$)%{%f%b%} '
+PS1='%b[%D{%m/%d %R:%S}] %B%~ ${_git_prompt}%(!.#.$)%{%f%b%} '
